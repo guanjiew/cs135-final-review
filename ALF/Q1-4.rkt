057966dec55e23649bd629a5895c8ad314b08ac5
Guanjie <guanjie.wang@mail.utoronto.ca>
2021-12-04 21:54:20 -0500

    ADD FILE

---
+++ ALF/Q1-4.rkt
@@ -0,0 +1,44 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname practice) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;; Produce true if the given number is prime; false otherwise
+;; is-prime?: Nat -> Boolean
+(define (is-prime? n)
+  (foldr (lambda (num cur_bool)
+                 (and (not (= 0 (remainder n num))) cur_bool))
+           true (range 2 n 1))
+  )
+
+(check-expect (is-prime? 143) #f)
+(check-expect (is-prime? 56) #f)
+(check-expect (is-prime? 17) #t)
+
+;; Sum all positive numbers in a list
+;; sum-pos: (listof integer) -> integer
+(define (sum-pos lon)
+  (foldr (lambda (num cur_sum)
+                (cond [(> num 0) (+ cur_sum num)]
+                      [else cur_sum])
+          ) 0 lon
+  )
+)
+(define test-list (list 1 -10 -5 7 9 11 13))
+(check-expect (sum-pos test-list) 41)
+
+;; increasing-list: Nat -> (listof Nat)
+
+(define (increasing-lists n)
+(build-list n (lambda (x)
+                (build-list (add1 x) identity))))
+(check-expect (list (list 0) (list 0 1) (list 0 1 2) (list 0 1 2 3) (list 0 1 2 3 4) (list 0 1 2 3 4 5) (list 0 1 2 3 4 5 6) (list 0 1 2 3 4 5 6 7)) (increasing-lists 8))
+
+;; Produce a diagnal matrix
+;; identity: Nat -> (listof (listof Integer))
+(define (identity-matrix n )
+  (build-list n (lambda (row)
+    (build-list n (lambda (col)
+                  (cond [(= row col) 1]
+                        [else 0]
+                        ))))))
+
+(check-expect (list (list 1 0 0 0 0) (list 0 1 0 0 0) (list 0 0 1 0 0) (list 0 0 0 1 0) (list 0 0 0 0 1))(identity-matrix 5))
